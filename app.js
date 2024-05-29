29671b522952e891dcd6a1f7cfd519bb801d9346
Alexander Gomez <alexgomezgomez15@gmail.com>
2024-05-29 11:24:52 -0400

    Nav Links created using Document Fragment

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const ulEl = document.querySelector("ul");
 
+const navLinks = ["About", "Images", "Suggestion"];
+
+const fragment = new DocumentFragment();
+
+for (const link of navLinks) {
+  const li = document.createElement("li");
+  li.textContent = link;
+  fragment.append(li);
+}
+
+ulEl.appendChild(fragment);
